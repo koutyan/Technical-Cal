08a58f87a16c0f271a5cec8303603db389e73ee6
Kosuke Sakai <kosukes332012ts@gmail.com>
2017-10-02 13:57:46 +0900

    Update sympthon_rule.java

--- sympthon_rule.java
+++ sympthon_rule.java
@@ -1,34 +1,43 @@
+//Created by Kosuke Sakai
+
 import java.lang.Math;
 import javax.swing.JOptionPane;
 
-public class sympthon {
+public class sympthon_rule {
 	public static void main(String[] args) {
 		double n = Double.parseDouble(
-				JOptionPane.showInputDialog("Input n"));
+				JOptionPane.showInputDialog("Input N")); //積分区間N等分
+		
+		///////適宜以下を変更///////
 		double a = 0;
 		double b = Math.PI;
+		/////////////////////////
 		
 		double h = (b-a)/n;
 		
-		double tms = Math.sin(a);
-		double tmf = Math.sin(b);
+		double tms = fx(a);
+		double tmf = fx(b);
 		double tm1 = 0;
 		double tm2 = 0;
 		
 		for (int i = 1; i <= n; i++){
 			if(i % 2 != 0){
-				tm1 = tm1 + Math.sin((b/n)*i);
+				tm1 = tm1 + fx(h*i);
 			}
 		}
 		for (int i = 1; i <= n; i++){
 			if(i % 2 == 0){
-				tm2 = tm2 + Math.sin((b/n)*i);
+				tm2 = tm2 + fx(h*i);
 			}
 		}
 		double ts = h * (tms + 4*tm1 + 2*tm2 + tmf) / 3;
 		
 		JOptionPane.showMessageDialog(null, ts);
 
+	}
+	
+	public static double fx(double x){
+		return Math.sin(x); //適宜ここを変更
 	}
 
 }
