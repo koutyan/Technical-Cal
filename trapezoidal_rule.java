da39677217e2871a1f738c23581fa137c34eb354
Kosuke Sakai <kosukes332012ts@gmail.com>
2017-10-02 13:52:43 +0900

    Update trapezoidal_rule.java

--- trapezoidal_rule.java
+++ trapezoidal_rule.java
@@ -1,29 +1,38 @@
+//Created by Kosuke Sakai
+
 import java.lang.Math;
 import javax.swing.JOptionPane;
 
 
-public class daikeiformula {
+public class trapezoidal_rule {
 
 	public static void main(String[] args) {
 		double n = Double.parseDouble(
-				JOptionPane.showInputDialog("Input n"));
+				JOptionPane.showInputDialog("Input n")); //積分区間n等分
+		
+		///////適宜ここを変更///////
 		double a = 0;
 		double b = Math.PI;
+		/////////////////////////
 		
 		double h = (b-a)/n;
 		
 		double tm = 0;
-		double tm1 = Math.sin(a)/2;
-		double tm2 = Math.sin(b)/2;
+		double tm1 = fx(a)/2;
+		double tm2 = fx(b)/2;
 		
 		for (int i = 0; i < n - 1; i++){
-			tm = tm + Math.sin((b/n)*(i+1));
+			tm = tm + fx(h * (i+1));
 		}
 		
-		double ts = h * (tm1+tm2+tm);
+		double ts = h * (tm1 + tm2 + tm);
 		
 		JOptionPane.showMessageDialog(null, ts);
 
+	}
+	
+	public static double fx(double x){
+		return Math.sin(x); //適宜ここを変更
 	}
 
 }
